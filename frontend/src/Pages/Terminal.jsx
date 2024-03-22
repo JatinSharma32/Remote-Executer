ba44114f72f36ffb2e12be1e7de9383cab852682
Jatin <jatinsharma9038@gmail.com>
2024-03-23 02:32:34 +0530

    dev: Prototype of Remote Code execution

--- frontend/src/Pages/Terminal.jsx
+++ frontend/src/Pages/Terminal.jsx
@@ -1,4 +1,43 @@
+import { useState } from "react";
+import Axios from "axios";
+
 const Terminal = () => {
-    return <div>Terminal</div>;
+    const [code, setCode] = useState("");
+
+    const handleOnChange = (event) => {
+        setCode(event.target.value);
+    };
+    const handleSubmit = () => {
+        Axios({
+            url: "http://localhost:4000/executor/",
+            method: "post",
+            data: { code: code },
+        })
+            .then((data) => {
+                console.log("Data sent: ", data);
+            })
+            .catch((err) => {
+                console.log("Error occured: ", err);
+            });
+    };
+
+    return (
+        <div>
+            <textarea
+                name="CodeEditor"
+                cols="30"
+                rows="12"
+                className="border-gray-300 w-full border-2 rounded-md"
+                value={code}
+                onChange={handleOnChange}
+            ></textarea>
+            <button
+                onClick={handleSubmit}
+                className="w-full my-2 text-white bg-gradient-to-bl from-slate-500 to-slate-800 py-3 px-4 rounded-md"
+            >
+                Submit
+            </button>
+        </div>
+    );
 };
 export default Terminal;
--- todo.md
+++ todo.md
@@ -13,7 +13,7 @@
 
 ---
 
--   Research this `exec()` method for executing various file codes and read [Code](https://github.com/Tushar3099/Remote-Executor/blob/main/rce-server/server/api/services/code.service.js).
+-   Research this `exec()` method for executing various file codes and read [Remote Ex Code](https://github.com/Tushar3099/Remote-Executor/blob/main/rce-server/server/api/services/code.service.js).
     ```js
     import { exec } from "child_process";
     exec("cd backend && npm run dev", function (error, stdout, stderr) {
@@ -24,7 +24,7 @@
         }
     });
     ```
--   Next start with the [Terminal page](https://www.npmjs.com/package/monaco-editor) and Fix the minor bugs in UI and
+-   Next start with the [Terminal page](https://blog.logrocket.com/build-web-editor-with-react-monaco-editor/#monaco-editor) and Fix the minor bugs in UI and
 -   User Authentication, add token checking on Main.jsx so user automatically gets logged in.
 -   Improve the backend architecture
 -   If issues faced while working with Text editor, we can ask user to upload their code files.
